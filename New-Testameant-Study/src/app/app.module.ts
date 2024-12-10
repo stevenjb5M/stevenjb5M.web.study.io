4822091c8a5fa0a8388194a96336563b3183b0df
Steven Brown <Stevenjbrown95@gmail.com>
2024-12-09 19:37:27 -0700

    stuff

--- New-Testameant-Study/src/app/app.module.ts
+++ New-Testameant-Study/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module'; // Import AppRoutingModule
+
+import { AppRoutingModule } from './app.routing.module'; // Import AppRoutingModule
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule,
     AppRoutingModule // Add AppRoutingModule to imports
   ],
   providers: [],
